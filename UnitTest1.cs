f74abaa6598156375d5e04a67f0a56401fe748fd
arnab212 <70711617+arnab212@users.noreply.github.com>
2021-07-29 12:52:00 +0530

    changed

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,21 +8,21 @@ namespace ForTest
     [TestFixture]
     public class Tests
     {
-        Mock<Banking> bank;
+        //Mock<Banking> bank;
        
         //Check C = new Check();
         Banking b = new Banking();
-        [SetUp]
-        public void Setup()
-        {
-            bank = new Mock<Banking>();
-        }
+        //[SetUp]
+        //public void Setup()
+        //{
+        //    bank = new Mock<Banking>();
+        //}
 
-        [Test]
-        public void Mtest()
-        {
-            bank.Setup(x => x.withdraw(1000)).Returns(9000);
-        }
+        //[Test]
+        //public void Mtest()
+        //{
+        //    bank.Setup(x => x.withdraw(1000)).Returns(9000);
+        //}
 
         //[Test]
         //public void Test1()
@@ -31,32 +31,32 @@ namespace ForTest
 
         //    Assert.AreEqual(7,res);
         //}
-        //[Test]
-        //public void Withdraw()
-        //{
-        //    int res = b.withdraw(9000);
-
-        //    Assert.AreEqual(1000, res);
-        //}
-        //[Test]
-        //public void Deposit()
-        //{
-        //    int res = b.deposit(9000);
-
-        //    Assert.AreEqual(19000, res);
-        //}
-        [Test]
-        public void checktrue()
+     
+        public void Withdraw()
         {
-            int res = b.demo(true);
-            Assert.AreEqual(42, res);
+            int res = b.withdraw(9000);
 
+            Assert.AreEqual(1000, res);
         }
         [Test]
-        public void checkfalse()
+        public void Deposit()
         {
-            Assert.Throws<InvalidOperationException>(() => b.demo(false));
+            int res = b.deposit(9000);
 
+            Assert.AreEqual(19000, res);
         }
+        //[Test]
+        //public void checktrue()
+        //{
+        //    int res = b.demo(true);
+        //    Assert.AreEqual(42, res);
+
+        //}
+        //[Test]
+        //public void checkfalse()
+        //{
+        //    Assert.Throws<InvalidOperationException>(() => b.demo(false));
+
+        //}
     }
 }
